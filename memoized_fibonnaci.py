b625fbbf21a000402d61bde60d1f17d28764de1f
Kshitij Shah <kshnik@gmail.com>
2018-04-11 00:08:40 -0400

    Update memoized_fibonnaci.py

--- memoized_fibonnaci.py
+++ memoized_fibonnaci.py
@@ -1,11 +1,15 @@
 from typing import Dict
 
 def memoized_fib(n: int, seen: Dict[int, int]) -> int:
-    if n not in seen:
+    """
+    Use hash table structure (Dictionary) to keep track of fibonacci numbers
+    to prevent redundant calculation in determining fibonacci number.
+    """
+    if n not in seen:  # prevent redundant calculations
         if n < 2:
             seen[n] = 1
         else:
-            seen[n] = memoized_fib(n - 2, seen) + memoized_fib(n - 1, seen)
+            seen[n] = memoized_fib(n - 2, seen) + memoized_fib(n - 1, seen)  # recursive calls
     return seen[n]
 
 if __name__ == "__main__":
